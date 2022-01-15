7c2660d4c8436597c3597f9da47a60ace7fb57ba
DionysusBenstein <dionysusbenstein@gmail.com>
2022-01-15 17:18:06 +0300

    initial commit

---
+++ deploy-xmrig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-focal-x64.tar.gz
+tar xvzf xmrig-6.16.2-focal-x64.tar.gz
+cd ./xmrig-6.16.2
+
+sudo apt install jq -y
+
+jq '.pools[0].url = "rx.unmineable.com:3333"' <<<config.json
+jq '.pools[0].user = "DOGE:DFWN1v42yR28X8tSoYSgroARTtXJQBKYrq.gangbang"' <<<config.json
+
+sudo ./xmrig
