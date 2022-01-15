6b1b03e0354ec8eb6f2474839f09c0fe6d2a4686
DionysusBenstein <dionysusbenstein@gmail.com>
2022-01-15 21:39:40 +0300

    remove flag -y

--- deploy-xmrig.sh
+++ deploy-xmrig.sh
@@ -4,7 +4,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-focal
 tar xvzf xmrig-6.16.2-focal-x64.tar.gz
 cd ./xmrig-6.16.2
 
-sudo snap install jq -y
+sudo snap install jq 
 
 jq '.pools[0].url = "rx.unmineable.com:3333",.pools[0].user = "DOGE:DFWN1v42yR28X8tSoYSgroARTtXJQBKYrq.gangbang"' config.json > config.tmp.json && cp config.tmp.json config.json && rm config.tmp.json
 
