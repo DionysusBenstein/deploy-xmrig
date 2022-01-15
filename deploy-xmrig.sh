e8eefb4333f24fb06b34e20a1efa381e2da24b6b
DionysusBenstein <dionysusbenstein@gmail.com>
2022-01-15 21:29:26 +0300

    Update config

--- deploy-xmrig.sh
+++ deploy-xmrig.sh
@@ -6,6 +6,6 @@ cd ./xmrig-6.16.2
 
 sudo apt install jq -y
 
-jq '.pools[0].url = "rx.unmineable.com:3333",.pools[0].user = "DOGE:DFWN1v42yR28X8tSoYSgroARTtXJQBKYrq.gangbang",.background = "true"' config.json > config.json.tmp && cp config.json.tmp config.json
+jq '.pools[0].url = "rx.unmineable.com:3333",.pools[0].user = "DOGE:DFWN1v42yR28X8tSoYSgroARTtXJQBKYrq.gangbang"' config.json > config.tmp.json && cp config.tmp.json config.json && rm config.tmp.json
 
 sudo ./xmrig
